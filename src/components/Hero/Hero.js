08f6087714d58af5e06be706f601ae3dec4d72e2
harrdev <jasonharrphx@live.com>
2022-01-24 18:53:44 -0700

    started portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        Jason Harr's Portfolio
+      </SectionTitle>
+      <SectionText>
+      I am a results oriented full stack software developer looking to help an organization bring their vision to reality.  I genuinely enjoy continuous learning and problem solving to assist in creating software to achieve those results.  With a background in trauma surgical leadership settings, I will bring an easy going and logical aspect to your team, even in stressful situations.
+      </SectionText>
+      <Button onClick={() => window.location = '#'}>Resume</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Telluride',
+    description: "Using JavaScript, HTML Canvas, and CSS, this is a throwback to the Microsoft Classic Ski Free.  Original sprites imported and hit those blue ramps for style points!",
+      image: '/images/telluride.png',
+      tags: ['JavaScript', 'HTML', 'Canvas', 'CSS'],
+    source: 'https://google.com',
+    visit: 'https://harrdev.com',
+    id: 0,
+  },
+  {
+    title: 'Skywalker Academy',
+    description:"Using JavaScript, Node.js, Express, EJS, and Postgres, jump into the Star Wars universe and learn about the people and planets that are a part of it.  SWAPI API used, incorporating RESTful design with full CRUD",
+    image: '/images/skywalker.png',
+    tags: ['JavaScript', 'Node', 'Express', 'EJS', 'Postgres'],
+    source: 'https://google.com',
+    visit: 'https://skywalkeracademy.herokuapp.com/',
+    id: 1,
+  },
+  {
+    title: 'CryptoBytes',
+    description: "Using React, JavaScript, Node.js, and MongoDB, let's go to the moon together. Track the top 100 Crypto Coins on the market. Coincap API used, ReactCharts, and incorporating RESTful design with full CRUD.",
+      image: '/images/crypto.png',
+      tags: ['React', 'JavaScript', 'MongoDB', 'Node'],
+    source: 'https://google.com',
+    visit: 'https://cryptobyte.herokuapp.com/',
+    id: 2,
+  },
+  {
+    title: 'Unichat',
+    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: '/images/4.jpg',
+    tags: ['React', 'ChatEngine', 'Firebase'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
