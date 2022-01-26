fecd8516c4fe1d5af3acb2c6cf9c01755199212f
harrdev <jasonharrphx@live.com>
2022-01-26 16:28:56 -0700

    added resume download functionality

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => (
         Jason Harr's Portfolio
       </SectionTitle>
       <SectionText>
-      I am a results oriented full stack software developer looking to help an organization bring their vision to reality.  I genuinely enjoy continuous learning and problem solving to assist in creating software to achieve those results.  With a background in trauma surgical leadership settings, I will bring an easy going and logical aspect to your team.
+        I am a results oriented full stack software developer looking to help an organization bring their vision to reality.  I genuinely enjoy continuous learning and problem solving to assist in creating software to achieve those results.  With a background in trauma surgical leadership settings, I will bring an easy going and logical aspect to your team.
       </SectionText>
       <a href='/resume/resume.docx' download><Button>Resume</Button></a>
     </LeftSection>
