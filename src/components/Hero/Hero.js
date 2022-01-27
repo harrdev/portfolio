426346746d4775409bf3cd66b92a41a7952312f2
harrdev <jasonharrphx@live.com>
2022-01-26 20:06:53 -0700

    changed resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => (
       <SectionText>
         I am a results oriented full stack software developer looking to help an organization bring their vision to reality.  I genuinely enjoy continuous learning and problem solving to assist in creating software to achieve those results.  With a background in trauma surgical leadership settings, I will bring an easy going and logical aspect to your team.
       </SectionText>
-      <a href='/resume/resume.docx' download><Button>Resume</Button></a>
+      <a href='/resume/resume.pdf' download><Button>Resume</Button></a>
     </LeftSection>
   </Section>
 );
