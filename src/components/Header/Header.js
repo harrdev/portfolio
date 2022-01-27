b872ae80b5dfd6a23637d608b3342f81b0bb4b27
harrdev <jasonharrphx@live.com>
2022-01-26 19:27:17 -0700

    potfolio v1 complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { VscRemote } from 'react-icons/vsc';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}>
-          <DiCssdeck size="3rem" /> <Span>HarrDev</Span>
+          <VscRemote size="3rem" /> <Span>HarrDev</Span>
         </a>
       </Link>
     </Div1>
